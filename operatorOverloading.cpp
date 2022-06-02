baad9e2bb25381ea9aa900e45a512351198e4f97
Chris Kayy <kckateera@gmail.com>
2022-06-03 01:42:23 +0200

    Add operator Overloading skeletal code.

---
+++ operatorOverloading.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Chris Kay on 2022/06/03.
+//
+
+#include <iostream>
+
+//>>, <<, +, -, /, *, +=, *=, -=,!=, ++, —, [], (), >,<,~
+namespace ClassMembers {
+    class Person {
+    public:
+    };
+}
+
+namespace NonClassMember{
+    class Person {
+    public:
+    };
+}
+
+
+int main(){
+
+}
