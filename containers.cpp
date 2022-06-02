e5d411d08fad565e62a7e644f92a445d566fb55c
Chris Kayy <kckateera@gmail.com>
2022-06-02 22:44:04 +0200

    Add vector container example.

---
+++ containers.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Chris Kay on 2022/06/02.
+//
+
+#include <memory>
+#include <iostream>
+#include <vector>
+using std::cout; using std::endl;
+
+void vectorIterators (void) {
+    int arr[4] = {4, 5, 6, 7};
+    std::vector<int> vectName(arr, arr + 4); // initialize with start and 1+end pointers.
+
+    typedef std::vector<int>::const_iterator iter; //make new typedef from old.
+
+    for (iter it = vectName.begin(); it != vectName.end(); it++) {
+        cout << *it << endl;
+    }
+    cout << endl;
+
+    vectName.push_back(8);
+
+    for (int &a: vectName) {
+        cout << a << endl;
+    }
+}
+
+int main(){
+    vectorIterators();
+}
+
