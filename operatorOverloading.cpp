1a072f9749a2ff2c538db350b855bd0369d95c18
Chris Kayy <kckateera@gmail.com>
2022-06-03 03:26:59 +0200

    Overload +, -, -=, ~, << , >>

--- operatorOverloading.cpp
+++ operatorOverloading.cpp
@@ -22,6 +22,27 @@ public:
         in >> rhs.netWorth >> rhs.age;
         return in;
     };
+    void operator~(void){ gender='F'; } // unary overload.
+    void operator+(int value){ netWorth+=value; }
+
+    Person operator+(const Person & rhs){
+        Person temp = *this;
+        temp.firstname += " " + rhs.firstname;
+        temp.middlename += " " + rhs.middlename;
+        temp.lastname += " " + rhs.lastname;
+        temp.age += rhs.age;
+        temp.netWorth += rhs.netWorth;
+        return temp;
+    }
+    friend Person & operator-=(Person & lhs ,const Person & rhs);
+private:
+    int netWorth;
+};
+
+// << operator can done outside class once friended.
+std::ostream & operator<<(std::ostream & out, const Person & rhs){
+    out << rhs.firstname + " " + rhs.middlename << " " << rhs.lastname << " " << rhs.age << " " << rhs.netWorth << " " << rhs.gender << endl;
+    return out;
 }
 
 
