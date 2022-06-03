ca3b99706fb8eeb0fd619b2bfc9aaf2f77797001
Chris Kayy <kckateera@gmail.com>
2022-06-03 04:47:55 +0200

    Add skeletal code for templates section

---
+++ templates.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Chris Kay on 2022/06/03.
+//
+
+namespace templates{
+
+}
+
+namespace stl{
+
+}
+
+it main(){
+    //templates, functors, lambdas, concepts (requires) - built in and own., traits.
+    //different parameter types, specialization,
+    //stl algorithms
+    //exceptions.
+}
